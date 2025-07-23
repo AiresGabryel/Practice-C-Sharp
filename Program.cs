11a0b455c9406f4e3ade93bfa5da367352231643
AiresGabryel <56Gabryel@gmail.com>
2025-07-22 23:57:29 -0300

    manipulação de variáveis

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Exemplo de uso de uma classe Pessoa
 using ExemploFundamentos.Models;
 
-
+/*
 //instanciando um objeto do tipo Pessoa
 Pessoa p = new Pessoa(); // p =  variável
 
@@ -12,3 +12,23 @@ p.Idade = 26;
 // chamando o método Apresentar
 p.Apresentar();
 
+*/
+
+
+///////////////////////////////
+
+
+//atribuindo valores diretamente às propriedades
+string apresentacao = "Olá, seja bem vindo";
+int quantidade = 1;
+double altura = 1.70;
+decimal preco = 1.80M;
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine($"Quantidade: {quantidade}");
+Console.WriteLine($"Altura: {altura.ToString("0.00")}");// double não utiliza duas casas decimais, converte para string com duas casas decimais
+Console.WriteLine($"Preço: {preco}");
+Console.WriteLine($"Boolean: {condicao}");
+
+// rodar o comando = dotnet run
