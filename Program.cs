a6103484ac8b8dc1910331a99c4901a81dc0602e
AiresGabryel <56Gabryel@gmail.com>
2025-07-23 00:52:32 -0300

    using DateTime Struct

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ p.Apresentar();
 
 ///////////////////////////////
 
-
+/*
 //atribuindo valores diretamente às propriedades
 string apresentacao = "Olá, seja bem vindo";
 int quantidade = 1;
@@ -30,5 +30,18 @@ Console.WriteLine($"Quantidade: {quantidade}");
 Console.WriteLine($"Altura: {altura.ToString("0.00")}");// double não utiliza duas casas decimais, converte para string com duas casas decimais
 Console.WriteLine($"Preço: {preco}");
 Console.WriteLine($"Boolean: {condicao}");
+*/
+
+// DateTime = estrutura que representa data e hora
+// .Now = propriedade que retorna a data e hora atual
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+
+// adiciona 5 dias à data atual
+DateTime dataAdd = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAdd);
+
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy")); // formata a data para o formato dia/mês/ano sem horário
+
 
 // rodar o comando = dotnet run
